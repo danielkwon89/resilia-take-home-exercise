36108a0c55663c63f281e5daf778f81a27caa08f
Daniel Kwon <danielkwon89@gmail.com>
2022-11-19 12:29:35 -0500

    created Notification seed data and console message when seed data is added successfully

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Notification.destroy_all
+
+notification1 = Notification.create!(title: 'Donation Thank You Letter', description: 'Send thank you letter to donors.')
+notification2 = Notification.create!(title: "Program Outcomes", description: "Create materials capturing program outcomes for board meetings and grant applications.")
+notification3 = Notification.create!(title: 'Fundraising Dept Meeting', description: 'Meeting to finalize details for upcoming fundraiser event.')
+notification4 = Notification.create!(title: "Grant Research", description: "Start research on grant applications for the upcoming year.")
+notification5 = Notification.create!(title: 'Marketing Strategy', description: 'Meeting to discuss marketing strategy for 2023.')
+notification6 = Notification.create!(title: 'Grant Applications', description: 'Complete remaining grant applications.')
+
+puts 'Successfully added seed data.'
