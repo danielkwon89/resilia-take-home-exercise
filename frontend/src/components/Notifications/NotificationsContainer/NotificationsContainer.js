d107b583de64f78ebba2cbf210e83594b253d1ab
Daniel Kwon <danielkwon89@gmail.com>
2022-11-19 18:07:42 -0500

    changing 'Notification Center' header from <h2> to <h1>

--- frontend/src/components/Notifications/NotificationsContainer/NotificationsContainer.js
+++ frontend/src/components/Notifications/NotificationsContainer/NotificationsContainer.js
@@ -3,7 +3,7 @@ import Notification from "../Notification/Notification"
 const NotificationsContainer = ({notifications}) => {
     return (
         <section className={'notification-center'}>
-            <h2>Notification Center</h2>
+            <h1>Notification Center</h1>
             {notifications && notifications.length ? notifications.map(notification => <Notification notification={notification}/>) : <h5>You have no notifications!</h5>}
         </section>
     )
