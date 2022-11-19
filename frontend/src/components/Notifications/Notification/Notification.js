3b2f65769b307bc03112e7fe9d3dfb207745025d
Daniel Kwon <danielkwon89@gmail.com>
2022-11-19 17:52:03 -0500

    adding isOpen state to Notification to toggle showing and hiding Notification description

--- frontend/src/components/Notifications/Notification/Notification.js
+++ frontend/src/components/Notifications/Notification/Notification.js
@@ -1,8 +1,27 @@
+import { useState } from "react";
+import Button from "../../Button/Button";
+
 const Notification = ({notification}) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDescription = (event) => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <article className={'notification'}>
             <h4>{notification && notification.title}</h4>
-            <p>{notification && notification.description}</p>
+            {isOpen ? (
+                <p className="notification-description">{notification.description}</p>
+            ) : (
+                <></>
+            )}
+
+            {isOpen ? (
+                <Button text={'Close'} callback={toggleDescription}/>
+            ) : (
+                <Button text={'View Details'} callback={toggleDescription}/>
+            )}
         </article>
     )
 }
